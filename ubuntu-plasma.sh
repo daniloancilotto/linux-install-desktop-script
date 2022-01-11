98ef85f11e7a18d0861556f3856dffa48ce11c47
Danilo Ancilotto <daniloancilotto@gmail.com>
2022-01-11 14:20:31 -0300

    Updated latte dock version

--- ubuntu-plasma.sh
+++ ubuntu-plasma.sh
@@ -127,11 +127,11 @@ printLine "Latte Dock"
 root_app_name="latte-dock"
 root_app_subdir="$root_app_dir/$root_app_name"
 root_app_cversion="`sudo cat "$root_app_subdir/version.txt"`"
-root_app_version="765aa45"
+root_app_version="837f0a8"
 
 home_app_name="latte-dock"
 home_app_subdir="$home_app_dir/$home_app_name"
-home_app_github_file="765aa45c72079b798018cc11ef77380264159b31"
+home_app_github_file="837f0a81e7c66a572cf1d738d9261c8c5e91a843"
 
 if [ "$root_app_cversion" != "$root_app_version" ]
 then
